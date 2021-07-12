f32ced210db40ab2fcab1fa26ff6f05858205fbe
ymoukhli <ymoukhli.1337@gmail.com>
2021-07-12 11:32:34 +0100

    still working on passions

--- src/components/Passions.js
+++ src/components/Passions.js
@@ -2,13 +2,31 @@ import passions from '../styles/Passions.module.css'
 import React , {useState} from 'react'
 import button from '../styles/buttons.module.css'
 
-let passionArray = ['music', 'wine', 'travel', 'sex', 'shopping', 'talking', 'running']
+let passionArray = ['music', 'wine', 'travel', 'sex', 'shopping', 'talking', 'running', 'dancing', 'gaming'];
+
+
 export const Passions = () => {
 
-    let [displayPassions, setCurrent] = useState(passionArray)
-    let currentPassions = displayPassions.map((x, i) => <div Key={i}className={passions.normal}>{x}</div>);
-    return (<div className={passions.root} onClick={(e) => console.log(e.currentTarget)}>
-        {currentPassions}
+    const Funy = (e) => {
+        console.log("hey"+display)
+        SetDisplay(!display);
+        console.log("hey"+e.currentTarget)
+        
+    }
+    let [displayPassions, setCurrent] = useState([''])
+    let [display, SetDisplay] = useState(false)
+
+    // buttons of current passions
+    let currentPassions = displayPassions.map((x, i) => <div Key={i + 'current'}className={passions.normal}>{x}</div>);
+
+    // buttons on overlay passions
+    let allPassions = passionArray.map((x, i) => <div Key={i + 'passions'}className={passions.normal}  onClick={(e) => {console.log(e.currentTarget)}}>{x} </div>);
+
+
+    return (
+        <div className={passions.root} onClick={Funy}>
+        {display && <div className={passions.overlay} onClick={Funy}>{allPassions}</div>}
+                {currentPassions}
         </div>);
 }
 
